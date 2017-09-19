23b6971e6c50e7aca36e354b441bd54361d2ac91
Joshua Ma <josh@benchling.com>
2017-09-18 22:30:24 -0700

    Add upload_results.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# api-examples
+# Benchling API Examples
 Short code snippets to illustrate working with the Benchling REST APi
+
+__This is a work in progress. Examples will be added / curated over time.__
---
+++ python/upload_results.py
@@ -0,0 +1,40 @@
+import os
+
+import requests
+
+# Read API_KEY from envvar
+api_key = os.environ['API_KEY']
+# If you have a private deploy, this may be YOURCOMPANY.benchling.com
+api_host = 'benchling.com'
+
+
+def api_get(endpoint):
+    rv = requests.get('https://%s%s' % (api_host, endpoint), auth=(api_key, ''))
+    return rv.json()
+
+
+def api_post(endpoint, params):
+    rv = requests.post('https://%s%s' % (api_host, endpoint), auth=(api_key, ''), json=params)
+    return rv.json()
+
+
+def main():
+    assay_schemas = api_get('/api/v2/assay-schemas')['assaySchemas']
+    result_schemas = [schema for schema in assay_schemas if schema['type'] == 'assay_result']
+    schema_id = result_schemas[0]['id']
+    
+    response = api_post('/api/v2/assay-results', [
+        {
+            'schemaId': schema_id,
+            'fields': {
+                'string_field': 'abc',
+                'int_field': 123,
+            },
+        },
+    ])
+    print 'Response:'
+    print repr(response)
+
+
+if __name__ == '__main__':
+    main()
